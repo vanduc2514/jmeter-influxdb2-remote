ab8ce9cb9a7eba00ceb09c7d4204d35eca99f3e0
vanduc2514 <vanduc2514@gmail.com>
2022-06-19 22:13:01 +0700

    Correct docker setup for integration setting

--- dev-tools/install.sh
+++
@@ -1,4 +0,0 @@
-export JMETER_LIB=/jmeter/apache-jmeter-5.4.1/lib
-
-docker cp ../../../influxdb2-remote/target/jmeter-influxdb2-remote-*.jar jmeter-master:$JMETER_LIB
-docker cp ../../../influxdb2-remote/target/jmeter-influxdb2-remote-*.jar jmeter-slave:$JMETER_LIB
