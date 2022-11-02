13ba30cab6bb5b91c8610a46e6621d4dd7e35f4d
vanduc2514 <vanduc2514@gmail.com>
2022-11-02 22:50:56 +0700

    - change project folder structure - correct pom.xml

--- dev-tools/docker/install.sh
+++ dev-tools/docker/install.sh
@@ -4,7 +4,7 @@ export JMETER_MASTER=jmeter-master
 
 docker stop $JMETER_SLAVE
 docker stop $JMETER_MASTER
-docker cp ../../influxdb2-remote/target/jmeter-influxdb2-remote-*.jar jmeter-master:$JMETER_LIB
-docker cp ../../influxdb2-remote/target/jmeter-influxdb2-remote-*.jar jmeter-slave:$JMETER_LIB
+docker cp ../../target/jmeter-influxdb2-remote-*.jar jmeter-master:$JMETER_LIB
+docker cp ../../target/jmeter-influxdb2-remote-*.jar jmeter-slave:$JMETER_LIB
 docker start $JMETER_SLAVE
 docker start $JMETER_MASTER
