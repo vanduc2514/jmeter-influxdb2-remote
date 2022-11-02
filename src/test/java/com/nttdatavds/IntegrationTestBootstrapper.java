a369049515df05f0d254c96ac038dbd7033bbcc1
vanduc2514 <vanduc2514@gmail.com>
2022-11-03 01:21:04 +0700

    - attempt to start client and server - remove unused file

---
+++ src/test/java/com/nttdatavds/IntegrationTestBootstrapper.java
@@ -0,0 +1,27 @@
+package com.nttdatavds;
+
+import org.apache.jmeter.JMeter;
+import org.apache.jmeter.engine.RemoteJMeterEngineImpl;
+import org.apache.jmeter.rmi.RMIServerSocketFactoryImpl;
+import org.apache.jmeter.rmi.RmiUtils;
+import org.apache.jmeter.rmi.SSLRMIServerSocketFactory;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.UnknownHostException;
+import java.rmi.RemoteException;
+import java.rmi.server.RMIServerSocketFactory;
+
+public class IntegrationTestBootstrapper {
+
+    public static void main(String[] args) throws IOException {
+        // Server start
+        RemoteJMeterEngineImpl.startServer(RmiUtils.getRmiRegistryPort());
+        // Client start
+        JMeter jMeter = new JMeter();
+        jMeter.start(new String[]{"command line"});
+
+    }
+
+}
